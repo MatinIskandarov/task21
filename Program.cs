8d9eb49dd2a411c8fb72afd844785f3a42ea5cbd
Matin-Iskandar <matin.iskenderov@gmail.com>
2022-11-21 06:24:28 +0400

    Created task21

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine(Book.GetDiscountedPrice(100, 10));
+    }
+
+   
+    
